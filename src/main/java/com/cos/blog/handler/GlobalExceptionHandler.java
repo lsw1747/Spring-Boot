d5d480e1e1ac8fc8063c21d80850db4d9b87f516
limgseungwoo <lsw17123@gmail.com>
2021-07-22 15:10:22 +0900

    메인화면 구성

--- src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.cos.blog.handler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+@ControllerAdvice
 @RestController
 public class GlobalExceptionHandler {
 	
+	@ExceptionHandler(value=IllegalArgumentException.class) //IllegalArgumentExcpetion에 대한 모든 에러는 이 컨트롤러로 매핑
 	public String HandleArgumentException(IllegalArgumentException e) {
 		
 		return "<h1>"+e.getMessage()+"</h1>";
